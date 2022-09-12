7fbbe26f374f0caaf0def589c4d6fe386c7298f0
Dillon Brock <mdillon1297@gmail.com>
2022-09-12 16:37:17 -0700

    added background image to main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,11 @@
 import './Main.css';
 import { zodiac } from '../../data';
 import ZodiacCard from '../ZodiacCard/ZodiacCard';
+import background from '../../background.jpg';
 
 export default function Main() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${background})` }}>
       {zodiac.map(sign => <ZodiacCard key={sign.id} {...sign}/>)}
     </main>
   );
