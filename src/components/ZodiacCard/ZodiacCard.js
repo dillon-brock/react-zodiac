9d342788e13c150f00087728fd0ca8edbf03a9b4
Dillon Brock <mdillon1297@gmail.com>
2022-09-12 15:13:06 -0700

    signs displaying, styling needs work

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,9 @@
-export default function ZodiacCard() {
+export default function ZodiacCard(props) {
   return (
-    <div className="zodiac-card"></div>
+    <div className="zodiac-card">
+      <img src={`${process.env.PUBLIC_URL}/zodiacs/${props.name}.png`} />
+      <h3>{props.name}</h3>
+      <span>{props.dates}</span>
+    </div>
   );
 }
